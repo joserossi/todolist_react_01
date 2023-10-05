115e4759ce6d8bee8c58dab2835fb99dc9c94099
jose <info@jose.com>
2023-10-05 19:54:45 -0300

    Add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+
+import { useState } from 'react'
 import { TaskCard } from './TaskCard'
 
 const tasks_list = [
@@ -16,22 +18,55 @@ const tasks_list = [
     task: 'Tarea 3',
     isDone: true,
     isActive: true
-  },
+  }
 ]
 
 function App() {
+  const [message, setMessage] = useState('')
+  const [updated, setUpdated] = useState(message)
+
+  const handleChange = (event) => {
+    setMessage(event.target.value)
+  }
+
+  const handleClick = () => {
+    // 👇 "message" stores input field value
+    setUpdated(message);
+    console.log(message)
+    tasks_list.push({
+      task: message,
+      isDone: false,
+      isActive: true}
+    )
+  };
   return (
     <section className='App'>
+      <div className='add-text'>
+        <input 
+          type="text" 
+          id='message'
+          name='message'
+          className='input-field'
+          onChange={handleChange}
+          value={message}
+        />
+        <button onClick={handleClick}>Agregar</button>
+      </div>
       {
         tasks_list.map(task_unit => {
           const { task, isDone, isActive } = task_unit
           return (
-            <TaskCard 
-              key= {task}
-              task= {task}
-              isDone= {isDone}
-              isActive= {isActive}
-              />
+            <>
+
+            <div>
+              <TaskCard 
+                key= {task}
+                task= {task}
+                isDone= {isDone}
+                isActive= {isActive}
+                />
+            </div>
+            </>
             )
         } )
       }
