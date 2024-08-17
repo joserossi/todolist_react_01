9f89ff2d17fd66fa395a02d16971e7f47daf5d06
jose <info@jose.com>
2024-08-16 23:08:06 -0300

    save data in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,96 @@
 import './App.css'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { TaskCard } from './TaskCard'
 
 const tasks_list = [
-  {
-    task: 'Tarea 1',
-    isDone: true,
-    isActive: true
-  },
-  {
-    task: 'Tarea 2',
-    isDone: false,
-    isActive: true
-  },
-  {
-    task: 'Tarea 3',
-    isDone: true,
-    isActive: true
-  }
 ]
 
 function App() {
   const [message, setMessage] = useState('')
-  const [updated, setUpdated] = useState(message)
+  // const [updated, setUpdated] = useState(message)
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem('tasks');
+    if (savedData) {
+      setJsonData(JSON.parse(savedData));
+    }
+  }, []);
 
   const handleChange = (event) => {
     setMessage(event.target.value)
   }
 
+  const handleClick_x = () => {
+    setMessage('')
+  }
+
+  const saveToLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks_list))
+  }
+
   const handleClick = () => {
-    // 👇 "message" stores input field value
-    setUpdated(message);
-    console.log(message)
+    // setUpdated(message);
+    // console.log(message)
     tasks_list.push({
       task: message,
       isDone: false,
       isActive: true}
     )
-  };
-  return (
-    <section className='App'>
-      <div className='add-text'>
-        <input 
-          type="text" 
-          id='message'
-          name='message'
-          className='input-field'
-          onChange={handleChange}
-          value={message}
-        />
-        <button onClick={handleClick}>Agregar</button>
-      </div>
-      {
-        tasks_list.map(task_unit => {
-          const { task, isDone, isActive } = task_unit
-          return (
-            <>
 
-            <div>
-              <TaskCard 
-                key= {task}
-                task= {task}
-                isDone= {isDone}
-                isActive= {isActive}
-                />
-            </div>
-            </>
-            )
-        } )
-      }
-    </section>
+    saveToLocalStorage()
+  };
+  
+  // const handleClick = () => {
+  //   // 👇 "message" stores input field value
+  //   setUpdated(message);
+  //   console.log(message)
+  //   tasks_list.push({
+  //     task: message,
+  //     isDone: false,
+  //     isActive: true}
+  //   )
+  // };
+  
+  return (
+    <div className="container">
+
+      <div className="container-child">
+        <section className='App'>
+          <div className='add-text'>
+            <input 
+              type="text" 
+              id='message'
+              name='message'
+              className='input-field'
+              onChange={handleChange}
+              value={message}
+            />
+            <button onClick={handleClick}>Add</button>
+            <button onClick={handleClick_x}>X</button>
+          </div>
+          {
+            jsonData.map(task_unit => {
+              const { task, isDone, isActive } = task_unit
+              return (
+                <>
+
+                <div>
+                  <TaskCard 
+                    key= {task}
+                    task= {task}
+                    isDone= {isDone}
+                    isActive= {isActive}
+                    />
+                </div>
+                </>
+                )
+            } )
+          }
+        </section>
+      </div>
+    </div>
   )
 }
 
